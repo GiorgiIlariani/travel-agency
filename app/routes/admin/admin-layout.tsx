9bb0af1046a9e8f042b48d278236c9cf9d14165a
Giorgi Ilariani <codemasterig@gmail.com>
2025-04-28 14:23:43 +0400

    implement routing, dashboard layout & ui, auth functionality

--- app/routes/admin/admin-layout.tsx
+++ app/routes/admin/admin-layout.tsx
@@ -1,9 +1,17 @@
+import { NavItems } from "components";
 import { Outlet } from "react-router";
+import { SidebarComponent } from "@syncfusion/ej2-react-navigations";
+import MobileSidebar from "components/MobileSidebar";
 
 const AdminLayout = () => {
   return (
     <div className="admin-layout">
-      <aside className="w-full max-w-[270px] hidden lg:block">Sidebar</aside>
+      <MobileSidebar />
+      <aside className="w-full max-w-[270px] hidden lg:block">
+        <SidebarComponent width={270} enableGestures={false}>
+          <NavItems />
+        </SidebarComponent>
+      </aside>
 
       <aside className="children">
         <Outlet />
--- app/routes/admin/all-users.tsx
+++ app/routes/admin/all-users.tsx
@@ -1,7 +1,15 @@
-import React from "react";
+import { Header } from "components";
 
 const AllUsers = () => {
-  return <div>USers Table</div>;
+  return (
+    <main className="dashboard wrapper">
+      <Header
+        title="Manage Users"
+        description="Filter, sort, and access detailed user profiles"
+      />
+      All Users Page Contents
+    </main>
+  );
 };
 
 export default AllUsers;
